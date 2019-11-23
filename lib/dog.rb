cb881719a838b30b1bc8f46215fb1259c5082f9f
Stephen Sanchez <stephensanchez05@gmail.com>
2019-11-23 19:43:52 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog 
+  def name=(dogs_name)
+    @the_dogs_name = dogs_name 
+  end 
+  
+  def name 
+    @name = the_dogs_name 
+  end 
+  
+  def breed=(dogs_breed)
+    @the_dogs_breed = dogs_breed
+  end
+  
+  def breed
+    @breed = the_dogs_breed
+  end
+end
+    
